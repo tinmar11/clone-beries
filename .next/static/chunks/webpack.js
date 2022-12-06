d647a81f9b72fe36ba5af4ad543149c03ab29a9a
0x110501 <108727583+0x110501@users.noreply.github.com>
2022-12-06 17:46:23 +0700

    footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "66a177653b5cc8fb"; }
+/******/ 		__webpack_require__.h = function() { return "3e0317d48a601bfb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.6ab3fdf8140277a5.hot-update.js
+++ .next/static/webpack/webpack.6ab3fdf8140277a5.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e973d24a3d01a39f"; }
+/******/ 	__webpack_require__.h = function() { return "3e0317d48a601bfb"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
